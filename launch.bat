772113744b33ae1305c6312838e442308ee2714e
maxmaxou2 <maxrossignol@hotmail.fr>
2021-04-15 02:13:09 +0200

    Auto-Update version deployment

--- launch.bat
+++ launch.bat
@@ -1,1 +1,12 @@
+@echo off
+setlocal EnableDelayedExpansion
 java -jar Prosper.jar
+set "condition=false"
+for %%f in (Prosper-*.jar) do (
+   del Prosper.jar
+   ren %%f Prosper.jar
+   set "condition=true"
+)
+if "%condition%"=="true" (
+   java -jar Prosper.jar
+)
